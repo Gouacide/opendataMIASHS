b883805618fed059189e3f6e243d6897a26586b3
MELGABSI <moezelgabsi@hotmail.fr>
2019-11-05 09:59:29 +0100

    add fetch function

--- myapp/index.js
+++ myapp/index.js
@@ -43,3 +43,27 @@ app.get('/names', function(req,res) {
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
   });
+  
+  
+  
+const fetch = require("node-fetch");
+	
+function correctMsg() {
+
+var request = require('request');
+request('https://api.jikan.moe/v3/search/anime?q=naruto', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body); // Print the google web page.
+  }
+});
+    }
+	
+function correctMsg2() {
+
+var request = require('request');
+request('https://cdn.animenewsnetwork.com/encyclopedia/api.xml?title=~naruto', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body); // Print the google web page.
+  }
+});
+    }
