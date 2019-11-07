1bbcc955906cfa5f909ecc9fe33f9fd5563c56b7
Romain <romain.bialek@hotmail.fr>
2019-11-07 12:25:46 +0100

    fixing synch problems

--- index.js
+++ index.js
@@ -50,20 +50,80 @@ app.get('/joke', function(req,res) {
 	});
 });
 
-app.get('/joke/:categories', function(req,res) {
-	console.log( req.params.categories);
+app.get('/joke/:categories', function(req,resp) {
+	
 	categories = req.params.categories;
-	correctMsg2(categories);
-	res.format({
-        'application/json': function () {
-		res.json(fetch_result);
-        },
-		'application/csv': function () {
-		const json2csvParser = new Parser({ fields, delimiter: ';' });
-		const csv = json2csvParser.parse(fetch_result);
-		res.send(Buffer.from(csv));
-		}
+	//**********************************
+	var unirest = require("unirest");
+console.log(categories);
+var req = unirest("GET", "https://jokeapi.p.rapidapi.com/category/"+categories);
+
+req.query({
+	"format": "json"
+});
+
+req.headers({
+	"x-rapidapi-host": "jokeapi.p.rapidapi.com",
+	"x-rapidapi-key": "Ez1zks3VaVmshNkRNA6CstvAj9ucp18lEoHjsnq0tTha5zioix"
+});
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+	var blague = res.body;
+	var blague_text = "";
+	console.log(blague);
+	if (blague["setup"]){blague_text= blague["setup"];
+	blague_text= blague_text+blague["delivery"]
+	}
+	else
+		blague_text=blague["joke"];
+	//console.log(blague_text);
+		
+		var unirest2 = require("unirest");
+
+		var req2 = unirest2("POST", "https://text-sentiment.p.rapidapi.com/analyze");
+
+		req2.headers({
+			"x-rapidapi-host": "text-sentiment.p.rapidapi.com",
+			"x-rapidapi-key": "Ez1zks3VaVmshNkRNA6CstvAj9ucp18lEoHjsnq0tTha5zioix",
+			"content-type": "application/x-www-form-urlencoded"
+		});
+
+		req2.form({
+			"text": blague_text
+		});
+
+		req2.end(function (res3) {
+			if (res3.error) throw new Error(res3.error);
+			var analyse = res3.body;
+			analyse=JSON.parse(analyse);
+			//console.log(analyse);
+			var emo ="";
+			if (analyse["pos_percent"] != "0%" )
+					emo = emo+" :) "; // &#x1F600;
+
+			if (analyse["mid_percent"] != "0%")
+					emo = emo+ " :| "; //&#x1F914;
+
+			if (analyse["neg_percent"] != "0%")
+					emo = emo+ " :( "; //&#x1F61F;
+			var result = JSON.stringify({blague : blague_text, categorie :blague["category"], emoji : emo });
+			fetch_result = JSON.parse(result);
+					resp.format({
+					'application/json': function () {
+					resp.json(fetch_result);
+					},
+					'application/csv': function () {
+					const json2csvParser = new Parser({ fields, delimiter: ';' });
+					const csv = json2csvParser.parse(fetch_result);
+					resp.send(Buffer.from(csv));
+					}
 	});
+		});
+});
+	
+	//**************************************************
+
 });
 
 				
